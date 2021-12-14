7dfcb73f518e58252b7acc7975c5e419a8d306c9
AlonDaniels2 <92639813+AlonDaniels2@users.noreply.github.com>
2021-12-14 17:53:20 +0200

    Add files via upload

---
+++ FibonacciHeap.java
@@ -0,0 +1,264 @@
+/**
+ * FibonacciHeap
+ *
+ * An implementation of a Fibonacci Heap over integers.
+ */
+public class FibonacciHeap
+{
+
+    private int size;
+    private int trees;
+    private int links;
+    private int cuts;
+    private int marked;
+    private HeapNode first;
+    private HeapNode min;
+
+    public FibonacciHeap() {
+        this.size = 0;
+        this.trees = 0;
+        this.links = 0;
+        this.cuts = 0;
+        this.marked = 0;
+        this.first = null;
+        this.min = null;
+    }
+
+   /**
+    * public boolean isEmpty()
+    *
+    * Returns true if and only if the heap is empty.
+    *
+    */
+    public boolean isEmpty()
+    {
+    	return this.size == 0;
+    }
+		
+   /**
+    * public HeapNode insert(int key)
+    *
+    * Creates a node (of type HeapNode) which contains the given key, and inserts it into the heap.
+    * The added key is assumed not to already belong to the heap.  
+    * 
+    * Returns the newly created node.
+    */
+    public HeapNode insert(int key)
+    {    
+    	return new HeapNode(key); // should be replaced by student code
+    }
+
+   /**
+    * public void deleteMin()
+    *
+    * Deletes the node containing the minimum key.
+    *
+    */
+    public void deleteMin()
+    {
+     	return; // should be replaced by student code
+     	
+    }
+
+   /**
+    * public HeapNode findMin()
+    *
+    * Returns the node of the heap whose key is minimal, or null if the heap is empty.
+    *
+    */
+    public HeapNode findMin()
+    {
+    	return this.min;
+    } 
+    
+   /**
+    * public void meld (FibonacciHeap heap2)
+    *
+    * Melds heap2 with the current heap.
+    *
+    */
+    public void meld (FibonacciHeap heap2)
+    {
+    	  return; // should be replaced by student code   		
+    }
+
+   /**
+    * public int size()
+    *
+    * Returns the number of elements in the heap.
+    *   
+    */
+    public int size()
+    {
+    	return this.size;
+    }
+    	
+    /**
+    * public int[] countersRep()
+    *
+    * Return an array of counters. The i-th entry contains the number of trees of order i in the heap.
+    * Note: The size of of the array depends on the maximum order of a tree, and an empty heap returns an empty array.
+    * 
+    */
+    public int[] countersRep()
+    {
+    	int[] arr = new int[100];
+        return arr; //	 to be replaced by student code
+    }
+	
+   /**
+    * public void delete(HeapNode x)
+    *
+    * Deletes the node x from the heap.
+	* It is assumed that x indeed belongs to the heap.
+    *
+    */
+    public void delete(HeapNode x) 
+    {    
+    	return; // should be replaced by student code
+    }
+
+   /**
+    * public void decreaseKey(HeapNode x, int delta)
+    *
+    * Decreases the key of the node x by a non-negative value delta. The structure of the heap should be updated
+    * to reflect this change (for example, the cascading cuts procedure should be applied if needed).
+    */
+    public void decreaseKey(HeapNode x, int delta)
+    {    
+    	return; // should be replaced by student code
+    }
+
+   /**
+    * public int potential() 
+    *
+    * This function returns the current potential of the heap, which is:
+    * Potential = #trees + 2*#marked
+    * 
+    * In words: The potential equals to the number of trees in the heap
+    * plus twice the number of marked nodes in the heap. 
+    */
+    public int potential() 
+    {    
+    	return this.trees + 2 * this.marked;
+    }
+
+   /**
+    * public static int totalLinks() 
+    *
+    * This static function returns the total number of link operations made during the
+    * run-time of the program. A link operation is the operation which gets as input two
+    * trees of the same rank, and generates a tree of rank bigger by one, by hanging the
+    * tree which has larger value in its root under the other tree.
+    */
+    public static int totalLinks()
+    {    
+    	return this.links;
+    }
+
+   /**
+    * public static int totalCuts() 
+    *
+    * This static function returns the total number of cut operations made during the
+    * run-time of the program. A cut operation is the operation which disconnects a subtree
+    * from its parent (during decreaseKey/delete methods). 
+    */
+    public static int totalCuts()
+    {    
+    	return this.cuts;
+    }
+
+     /**
+    * public static int[] kMin(FibonacciHeap H, int k) 
+    *
+    * This static function returns the k smallest elements in a Fibonacci heap that contains a single tree.
+    * The function should run in O(k*deg(H)). (deg(H) is the degree of the only tree in H.)
+    *  
+    * ###CRITICAL### : you are NOT allowed to change H. 
+    */
+    public static int[] kMin(FibonacciHeap H, int k)
+    {    
+        int[] arr = new int[100];
+        return arr; // should be replaced by student code
+    }
+    
+   /**
+    * public class HeapNode
+    * 
+    * If you wish to implement classes other than FibonacciHeap
+    * (for example HeapNode), do it in this file, not in another file. 
+    *  
+    */
+    public static class HeapNode{
+
+    	private int key;
+        private boolean marked;
+        private int rank;
+        private HeapNode child;
+        private HeapNode next;
+        private HeapNode prev;
+        private HeapNode parent;
+
+    	public HeapNode(int key) {
+    		this.key = key;
+            this.marked = false;
+            this.rank = 0;
+            this.child = null;
+            this.next = null;
+            this.prev = null;
+            this.parent = null;
+    	}
+
+    	public int getKey() {
+    		return this.key;
+    	}
+
+        public boolean getMarked() {
+            return this.marked;
+        }
+
+        public void setMarked(boolean mark) {
+            this.marked = mark;
+        }
+
+        public int getRank() {
+            return this.rank;
+        }
+
+       public void setRank(int rank) {
+           return this.rank = rank;
+       }
+
+       public HeapNode getChild() {
+           return this.child;
+       }
+
+       public void setChild(HeapNode node) {
+           return this.child = node;
+       }
+
+       public HeapNode getNext() {
+           return this.next;
+       }
+
+       public void setNext(HeapNode node) {
+           return this.next = node;
+       }
+
+       public HeapNode getPrev() {
+           return this.prev;
+       }
+
+       public void setPrev(HeapNode node) {
+           return this.prev = node;
+       }
+
+       public HeapNode getParent() {
+           return this.parent;
+       }
+
+       public void setParent(HeapNode node) {
+           return this.parent = node;
+       }
+    }
+}
